2d95a3b7cd3763efccb40a74e34e84d2a838a3d6
Umair <uahmed@eddress.com>
2022-11-27 23:33:19 +0500

    Stopping the service when app is in background and resuming again when app in foreground

--- app/src/main/java/com/example/backgroundlocation/LocationApp.kt
+++ app/src/main/java/com/example/backgroundlocation/LocationApp.kt
@@ -10,7 +10,7 @@ class LocationApp : Application() {
     override fun onCreate() {
         super.onCreate()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel("location", "Location", NotificationManager.IMPORTANCE_HIGH)
+            val channel = NotificationChannel("location", "Location", NotificationManager.IMPORTANCE_LOW)
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
--- app/src/main/java/com/example/backgroundlocation/MainActivity.kt
+++ app/src/main/java/com/example/backgroundlocation/MainActivity.kt
@@ -10,24 +10,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.example.backgroundlocation.ui.services.LocationService
 import com.example.backgroundlocation.ui.theme.BackgroundLocationTheme
 
 class MainActivity : ComponentActivity() {
+    private val LOCATION_PERMISSION_CONSTANT = 11
+    private var IS_SERVICE_STOPPED_MANUALLY = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ActivityCompat.requestPermissions(
             this,
             arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
-            11
+            LOCATION_PERMISSION_CONSTANT
         )
 
         setContent {
@@ -56,14 +55,32 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-
-/*
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BackgroundLocationTheme {
-
+    override fun onResume() {
+        super.onResume()
+        if (IS_SERVICE_STOPPED_MANUALLY) {
+            IS_SERVICE_STOPPED_MANUALLY = false
+            Intent(applicationContext, LocationService::class.java).apply {
+                action = LocationService.ACTION_START
+                startService(this)
+            }
+        }
     }
-}*/
+
+    override fun onPause() {
+        IS_SERVICE_STOPPED_MANUALLY = true
+        Intent(applicationContext, LocationService::class.java).apply {
+            action = LocationService.ACTION_STOP
+            startService(this)
+        }
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        Intent(applicationContext, LocationService::class.java).apply {
+            action = LocationService.ACTION_STOP
+            startService(this)
+        }
+        super.onDestroy()
+    }
+}
